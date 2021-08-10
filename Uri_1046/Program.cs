2aca24fad2be9c35025ba4bcf5ef19e2c916b236
RaulORios <raul.orios89@outlook.com>
2021-08-10 13:32:56 -0300

    Arquivos adicionados

---
+++ Uri_1046/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uri_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int hora_inicio = int.Parse(valores[0]);
+            int hora_fim = int.Parse(valores[1]);
+
+            int duracao;
+
+            if (hora_inicio < hora_fim)
+            {
+                duracao = hora_fim - hora_inicio;
+            }
+            else
+            {
+                duracao = 24 - hora_inicio + hora_fim;
+            }
+                                    
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+            
+        }
+    }
+}
